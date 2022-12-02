aa9cdd9ec1deb290ffed7b8ed8424777a9d3ec61
Andy Ogzewalla <29266+WizardOfOgz@users.noreply.github.com>
2022-12-02 17:20:11 -0600

    refactor Day 2 Haskell solution

--- 2022/day-2.hs
+++ 2022/day-2.hs
@@ -2,71 +2,73 @@ import Data.List.Split (splitOn)
 import Data.Map (Map, (!))
 import qualified Data.Map as Map
 
+import Data.Typeable (typeOf)
+
+
 pts = Map.fromList [
-    ("X", 1),
-    ("Y", 2),
-    ("Z", 3)
+    ('X', 1),
+    ('Y', 2),
+    ('Z', 3)
   ]
 
 battle_score = Map.fromList [
-    ("A", Map.fromList [
-        ("X", 3),
-        ("Y", 6),
-        ("Z", 0)
+    ('A', Map.fromList [
+        ('X', 3),
+        ('Y', 6),
+        ('Z', 0)
       ]),
-    ("B", Map.fromList [
-        ("X", 0),
-        ("Y", 3),
-        ("Z", 6)
+    ('B', Map.fromList [
+        ('X', 0),
+        ('Y', 3),
+        ('Z', 6)
       ]),
-    ("C", Map.fromList [
-        ("X", 6),
-        ("Y", 0),
-        ("Z", 3)
+    ('C', Map.fromList [
+        ('X', 6),
+        ('Y', 0),
+        ('Z', 3)
       ])
   ]
 
 battle_choice = Map.fromList [
-    ("A", Map.fromList [
-        ("X", "Z"),
-        ("Y", "X"),
-        ("Z", "Y")
+    ('A', Map.fromList [
+        ('X', 'Z'),
+        ('Y', 'X'),
+        ('Z', 'Y')
       ]),
-    ("B", Map.fromList [
-        ("X", "X"),
-        ("Y", "Y"),
-        ("Z", "Z")
+    ('B', Map.fromList [
+        ('X', 'X'),
+        ('Y', 'Y'),
+        ('Z', 'Z')
       ]),
-    ("C", Map.fromList [
-        ("X", "Y"),
-        ("Y", "Z"),
-        ("Z", "X")
+    ('C', Map.fromList [
+        ('X', 'Y'),
+        ('Y', 'Z'),
+        ('Z', 'X')
       ])
   ]
 
 result_pts = Map.fromList [
-    ("X", 0),
-    ("Y", 3),
-    ("Z", 6)
+    ('X', 0),
+    ('Y', 3),
+    ('Z', 6)
   ]
 
 filterEmptyString :: [String] -> [String]
 filterEmptyString = filter ((> 0) . length)
 
-parseInput :: String -> [[String]]
-parseInput = map (splitOn " ") . filterEmptyString . splitOn "\n"
+parseInput :: String -> [[Char]]
+parseInput = filterEmptyString . splitOn "\n"
 
-processWith :: ([String] -> Integer) -> [[String]] -> Integer
+processWith :: ([Char] -> Integer) -> [[Char]] -> Integer
 processWith f = foldl (\ total dat -> total + f dat) 0
 
-weaponChoice (opponentChoice:yourChoice:[]) = pts ! yourChoice + battle_score ! opponentChoice ! yourChoice
+weaponChoice (opponentChoice:' ':yourChoice:[]) = pts ! yourChoice + battle_score ! opponentChoice ! yourChoice
 
-outcomeChoice (opponentChoice:desiredOutcome:[]) = pts ! (battle_choice ! opponentChoice ! desiredOutcome) + result_pts ! desiredOutcome
+outcomeChoice (opponentChoice:' ':desiredOutcome:[]) = pts ! (battle_choice ! opponentChoice ! desiredOutcome) + result_pts ! desiredOutcome
 
 main = do
   contents <- readFile $ "day-2-input.txt"
   let input = parseInput contents
-
 
   let result1 = processWith weaponChoice input
   print $ "Part 1: " ++ show (result1)
